b22757c6eb7c8be1ff6ef7b5fc36d021cd236373
Guy <guydaane@gmail.com>
2020-02-10 15:56:17 +0200

    app style moved to globalStyle.js

--- src/App.js
+++ src/App.js
@@ -5,34 +5,12 @@ import { createStore, applyMiddleware } from 'redux'
 import reducers from './redux/reducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-import { createGlobalStyle } from 'styled-components'
 import { GameScreen } from './components/screens/GameScreen'
 import { initialState } from './consts/data/initialState'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './redux/reducers'
 
-const cursorImage = require('../src/images/cursor.png')
-
-const GlobalStyle = createGlobalStyle`
-* {
-  cursor: url(${cursorImage}), default;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-}
-
-div {
-  box-sizing: border-box;
-}
-
-img {
-    // image-rendering: pixelated;
-    image-rendering: -moz-crisp-edges;
-    image-rendering: crisp-edges;
-  }
-`
+import { GlobalStyle } from './components/globalStyle'
 
 export default function App() {
   const sagaMiddleware = createSagaMiddleware()
